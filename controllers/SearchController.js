ea7fe43f032bbaa866ddadf38b12edf02934b29c
Salah0123 <hossameldinsalah67@gmail.com>
2023-07-03 19:37:01 +0200

    By Hossam Hassan

--- controllers/SearchController.js
+++ controllers/SearchController.js
@@ -3,12 +3,52 @@ const Product = require("./../models/productSchema");
 
 
 
+// exports.searchFunc = (req, res, next) => {
+//   const { page = 1, limit = 10, search, category = "All" } = req.query;
+//   Product.find({
+//     $or: [
+//       { name: { $regex: search } },
+//           // { price: { $regex: search } },
+//     ],
+//     //   "category.name": "women's clothes",
+//   })
+//     .populate("category", "name")
+//     .then((doc) => {
+//       res.status(200).json({ doc });
+//     })
+//     .catch((err) => next(err));
+// };
+
+// exports.filterFunc = (req, res, next) => {
+//   const { color, size, price, model } = req.query;
+//   Product.find({
+//     $or: [
+//       { color: { $regex: color } },
+//       { size: { $regex: size } },
+//       { model: { $regex: model } },
+//       { price: { $lte: price } },
+//     ],
+//   })
+//     .then((doc) => {
+//       if (doc.length >= 1) {
+//         res.status(200).json({ doc });
+//       } else {
+//         const error = new Error("No Product Found");
+//         next(error);
+//       }
+//     })
+//     .catch((error) => next(error));
+// };
+
+
+
+
 exports.searchFunc = (req, res, next) => {
   const { page = 1, limit = 10, search, category = "All" } = req.query;
   Product.find({
     $or: [
       { name: { $regex: search } },
-          // { price: { $regex: search } },
+      // { price: { $regex: search } },
     ],
     //   "category.name": "women's clothes",
   })
@@ -21,14 +61,27 @@ exports.searchFunc = (req, res, next) => {
 
 exports.filterFunc = (req, res, next) => {
   const { color, size, price, model } = req.query;
-  Product.find({
-    $or: [
-      { color: { $regex: color } },
-      { size: { $regex: size } },
-      { model: { $regex: model } },
-      { price: { $lte: price } },
-    ],
-  })
+  const query = {};
+  const orArray = [];
+
+  if (color) {
+    orArray.push({ color: { $regex: color } });
+  }
+  if (size) {
+    orArray.push({ size: { $regex: size } });
+  }
+  if (model) {
+    orArray.push({ model: { $regex: model } });
+  }
+  if (price && !isNaN(price)) {
+    orArray.push({ price: { $lte: price } });
+  }
+
+  if (orArray.length > 0) {
+    query.$or = orArray;
+  }
+
+  Product.find(query)
     .then((doc) => {
       if (doc.length >= 1) {
         res.status(200).json({ doc });
